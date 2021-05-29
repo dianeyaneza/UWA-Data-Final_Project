1fe890fe0b729d4e997147f982b3ee8b3c493ab2
dianeyaneza <73869768+dianeyaneza@users.noreply.github.com>
2021-05-29 11:33:58 +0800

    updated overlays

---
+++ assets/js/overlay.js
@@ -0,0 +1,11 @@
+function ProfilePage() {
+    document.getElementById("ProfileOverlay").style.display = "block";
+}
+function ResultsPage() {
+    document.getElementById("ResultsOverlay").style.display = "block";
+}
+function off() {
+    document.getElementById("ProfileOverlay").style.display = "none";
+    document.getElementById("ResultsOverlay").style.display = "none";
+}
+
