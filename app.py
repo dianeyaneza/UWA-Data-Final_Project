d5526f8bb36f3d29669ebe3ae6ae058fe0de345b
dianeyaneza <73869768+dianeyaneza@users.noreply.github.com>
2021-06-02 21:03:08 +0800

    flask app rendering template. need to convert df to json

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
 # Dependencies
-from flask import (
-    Flask, 
-    render_template, 
-    request, 
-    redirect, 
-    jsonify)
+from flask import Flask, render_template, request, redirect, jsonify
 from models import create_classes
 import sqlalchemy
 import pandas as pd
@@ -20,17 +15,17 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql' + os.environ.get('DATABASE_URL', '')[8:]  or "sqlite:///db.sqlite"
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql' + os.environ.get('DATABASE_URL', '')[8:]  or "sqlite:///db.sqlite"
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
 
 # Remove tracking modifications
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
-wmhdata = create_classes(db)
+# wmhdata = create_classes(db)
 
 # create route that renders index.html template
 @app.route("/")
@@ -39,20 +34,25 @@ def home():
 
     
 # END POINT: JSON WORLD MH DATA
-@app.route("/wmwbata")
-def events():
+@app.route("/wmhdata")
+def data():
     # See events_flask.ipynb for the same code at this end point, showing the variable outputs throughout the steps. 
 
     ######### CONNECT TO DATABASE AND READ DATA AS DATAFRAME VIA PANDAS #########
     # Step 1. ##### Connect to postgres database and save to variable 'engine' #####
 
     ### Option 1: For postgres users
-    rds_connection_string = "postgres:postgres@localhost:5432/events_db"
+    # rds_connection_string = "postgres:postgres@localhost:5432/events_db"
     
     ### Option 2: For postgres users to enter in personal login details (if option1 does not work)
     rds_connection_string = "postgres:309Malanday!@localhost:5432/events_db"
 
     engine = create_engine(f'postgresql://{rds_connection_string}')
+
+    # Step 2. save data into a pandas variable using engine 
+    wmhdata = pd.read_sql_table('whatdoyoumean_table', engine) 
+
+    # Step 3. Convert pandas df to json
 
 
 
