60f97b4e386fa61a92a875940318746382b5f880
dianeyaneza <73869768+dianeyaneza@users.noreply.github.com>
2021-06-03 18:46:57 +0800

    reset map removed code that made map disappear

--- static/js/age.js
+++ static/js/age.js
@@ -4,30 +4,6 @@ function updateMap() {
         // console.log(data);
         var data = data;
         // createAnxiety(data);
-
-        // coordinates
-        var lat = data.map(elem => elem.latitude);
-        var long = data.map(elem => elem.longitude);
-        var latlong = lat.map(function(latitude, index){
-            return [latitude, long[index]];
-            // console.log(latlong)
-        });
-
-        // Anxiety by age group
-        var ab5 = data.map(elem => elem.A_below_5yo);
-        var aa5 = data.map(elem => elem.A_above_5yo);
-        var aa14 = data.map(elem => elem.A_above_14yo);
-        var aa49 = data.map(elem => elem.A_above_49yo);
-        var aa69 = data.map(elem => elem.A_above_69yo);
-        // console.log(ab5)
-
-        // Depression by age group
-        var db5 = data.map(elem => elem.D_below_5yo);
-        var da5 = data.map(elem => elem.D_above_5yo);
-        var da14 = data.map(elem => elem.D_above_14yo);
-        var da49 = data.map(elem => elem.D_above_49yo);
-        var da69 = data.map(elem => elem.D_above_69yo);
-        // console.log(db5)
         
         // age dropdown
         var ageOptions = ["Under 5", "5-14", "15-49", "50-69", "Over 70"]
@@ -39,30 +15,54 @@ function updateMap() {
             .append('option')
             .text(function (d) { return d; }) // text showed in the dropdown
             .attr("value", function (d) {return d;})
-        });
 
         // Event listener for age dropdown selection 
-        d3.select("#selAge").on("change", function(selectedOption) {
+        d3.select("#selAge").on("change", function(selectedAge) {
             
-            var selectedOption = d3.select(this).property("value")
-            console.log(selectedOption); 
+            var selectedAge = d3.select(this).property("value")
+            console.log(selectedAge); 
+            // console.log(data);
             
-            // filtData = data.filter(data => data.category == selectedOption);
-            // console.log(filtData);
+            // Anxiety by age group
+            var ab5 = data.map(elem => elem.A_below_5yo);
+            var aa5 = data.map(elem => elem.A_above_5yo);
+            var aa14 = data.map(elem => elem.A_above_14yo);
+            var aa49 = data.map(elem => elem.A_above_49yo);
+            var aa69 = data.map(elem => elem.A_above_69yo);
+            // console.log(ab5)
 
-            // // RUN FUNCTIONS // 
-            updateMap(selectedOption); 
-            // updateTable(); // The code for this function is located in 3_table.js file
+            if (selectedAge == "Under 5") {
+                filtA= data.map(elem => elem.D_below_5yo);
+            }
+            if (selectedAge == "5-14") {
+                filtA =  data.map(elem => elem.D_above_5yo);
+            }
+            console.log(filtA);
 
-        
+            var lat = data.map(elem => elem.latitude);
+            var long = data.map(elem => elem.longitude);
+            var latlong = lat.map(function(latitude, index){
+                return [latitude, long[index]];
+            });
 
-        // function updateMap(selectedGroup){
+            console.log(latlong);
 
+            // Depression by age group
+            // var db5 = data.map(elem => elem.D_below_5yo);
+            // var da5 = data.map(elem => elem.D_above_5yo);
+            // var da14 = data.map(elem => elem.D_above_14yo);
+            // var da49 = data.map(elem => elem.D_above_49yo);
+            // var da69 = data.map(elem => elem.D_above_69yo);
+            // console.log(db5)
+
+            // filtA = data.filter(data => data == selectedAge);
+            // console.log(filtA);
+
+            // RUN FUNCTIONS // 
+            // updateMap(filtA); 
             
+        });
 
-
-            
-        // }
     });
 };
 
--- static/js/app.js
+++ static/js/app.js
@@ -1,47 +1,9 @@
 d3.json('/api_wmhdata').then(function(data) {
     console.log(data);
-    createAnxiety(data);
+    // createAnxiety(data);
 })
 
-function createAnxiety(data) {
-    function onEachEntity(entity, layer) {
-        layer.bindPopup("<h3>" + data.Entity 
-        );
-    }
 
-    // Draw marker radius
-    function markerRadius(selectedAge) {
-        return magnitude * 30000; 
-    }
-    
-    // Draw marker colours
-    function markerColour(selectedAge) {
-        // Magnitude up to 1 
-        if (magnitude < 1) {
-            return "#E6E696"
-        }
-        // Magnitude 1 to 2
-        else if (magnitude < 2) {
-            return "yellow"
-        }
-        // Magnitude 2 to 3
-        else if (magnitude < 3) {
-            return "orange"
-        }
-        // Magnitude 3 to 4
-        else if (magnitude < 4) {
-            return "red"
-        }
-        // Magnitude 4 to 5
-        else if (magnitude < 5) {
-            return "maroon"
-        }
-        // Magnitude 5 and up
-        else {
-            return "brown"        
-        }
-
-}
 
 var mymap = L.map('map').setView([10.82, 21.80], 2.5);
 
@@ -53,19 +15,15 @@ var streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiZGlhbmV5YW5lemEiLCJhIjoiY2tuZnZvenkyMXZyeDJvazg3NHlvNGJoMSJ9.b2Mbk6QyGrdG8GIqJr1lNw'
 }).addTo(mymap);
-
 var cities = L.layerGroup();
 L.marker([-31.95, 115.86]).bindPopup('This is Perth.').addTo(cities),
     L.marker([-26.20, 28.04]).bindPopup('This is Johannesburg.').addTo(cities),
     L.marker([23.11, -82.36]).bindPopup('This is Havana').addTo(cities);
-
 var baseMaps = {
     "Streets": streets
 };
-
 var overlayMaps = {
     "Anxiety": cities
-
-};
+}
 
 L.control.layers(overlayMaps).addTo(mymap);
