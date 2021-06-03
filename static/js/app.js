f1cd4e9fd997fa88eb60ab8f2f7cfe1b4af0bee6
dianeyaneza <73869768+dianeyaneza@users.noreply.github.com>
2021-06-03 19:35:19 +0800

    markers with layergroups now appearing

--- static/js/app.js
+++ static/js/app.js
@@ -10,41 +10,51 @@ d3.json('/api_wmhdata').then(function(data) {
     });
     console.log(latlong);
     // createAnxiety(data);
-});
 
-var mymap = L.map('map').setView([10.82, 21.80], 2.5);
 
-var streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiZGlhbmV5YW5lemEiLCJhIjoiY2tuZnZvenkyMXZyeDJvazg3NHlvNGJoMSJ9.b2Mbk6QyGrdG8GIqJr1lNw'
-}).addTo(mymap);
+    var mymap = L.map('map').setView([10.82, 21.80], 2.5);
 
-//  new code to add countries coordinates marker
-var layerGroup = L.layerGroup().addTo(map);
+    var streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1IjoiZGlhbmV5YW5lemEiLCJhIjoiY2tuZnZvenkyMXZyeDJvazg3NHlvNGJoMSJ9.b2Mbk6QyGrdG8GIqJr1lNw'
+    }).addTo(mymap);
 
-for (i = 0; i < latlong.length; i++) {
-    marker = L.marker([coordinates[i][0], coordinates[i][1]]);
-    layerGroup.addLayer(marker);
-}
-
-var overlay = {'markers': layerGroup};
-L.control.layers(null, overlay).addTo(map);
-
-// var countries = L.layerGroup();
-// L.marker([-31.95, 115.86]).bindPopup('This is Perth.').addTo(cities),
-//     L.marker([-26.20, 28.04]).bindPopup('This is Johannesburg.').addTo(cities),
-//     L.marker([23.11, -82.36]).bindPopup('This is Havana').addTo(cities);
 
     var baseMaps = {
-    "Streets": streets
-};
+        "Streets": streets
+    };
 
-var overlayMaps = {
-    "Anxiety": cities
-}
+    // var countries = L.layerGroup();
+    // L.marker([-31.95, 115.86]).bindPopup('This is Perth.').addTo(cities),
+    //     L.marker([-26.20, 28.04]).bindPopup('This is Johannesburg.').addTo(cities),
+    //     L.marker([23.11, -82.36]).bindPopup('This is Havana').addTo(cities);
 
-L.control.layers(overlayMaps).addTo(mymap);
+    // var overlayMaps = {
+    //     "Anxiety": cities
+    // }
+
+    //  new code to add countries coordinates marker
+    var aData = L.layerGroup().addTo(mymap);
+    for (i = 0; i < latlong.length; i++) {
+        marker = L.marker([latlong[i][0], latlong[i][1]]);
+        aData.addLayer(marker);
+    }
+    var dData = L.layerGroup().addTo(mymap);
+    for (i = 0; i < latlong.length; i++) {
+        marker = L.marker([latlong[i][0], latlong[i][1]]);
+        dData.addLayer(marker);
+    }
+    var overlayMaps = {
+        'Anxiety': aData,
+        'Depression': dData
+
+    };
+
+    // layer control on top right
+    L.control.layers(overlayMaps).addTo(mymap);
+
+});
